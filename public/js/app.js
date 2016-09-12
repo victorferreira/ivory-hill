b977dfa35c3f561bdf9d1437d204e78a0cb603f3
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2016-09-12 11:41:33 -0300

    add firebase event listener fo add, remove, adn change

--- public/js/app.js
+++ public/js/app.js
@@ -141,15 +141,35 @@
     storageBucket: "ivory-hill.appspot.com",
   };
   firebase.initializeApp(config);
-  
+
   // Get elements
   const preObject = document.getElementById('object');
-  
+	const ulList = document.getElementById('list')
+
   // Create Reference
   const dbRefObject = firebase.database().ref().child('object');
-  
+	const dbRefList = dbRefObject.child('hobbies');
+
   // Sync object changes
   dbRefObject.on('value', snap => {
     preObject.innerText = JSON.stringify(snap.val(), null, 3);
   });
+
+	// Sync list changes
+	dbRefList.on('child_added', snap => {
+		const li = document.createElement('li');
+		li.id = snap.key;
+		li.innerText = snap.val();
+		ulList.appendChild(li);
+	});
+
+	dbRefList.on('child_changed', snap => {
+		const liChanged = document.getElementById(snap.key);
+		liChanged.innerText = snap.val();
+	});
+
+	dbRefList.on('child_removed', snap => {
+		const liToRemove = document.getElementById(snap.key);
+		liToRemove.remove();
+	});
 }());
